698c88b4bc38829c02ffce80b4d7ad74f0c039cd
Josh Kwok <toeinriver@Joshs-MacBook-Pro.local>
2019-07-29 11:18:10 +0800

    first commit

---
+++ app/src/main/java/com/neonxorg/neonx/MainActivity.java
@@ -0,0 +1,57 @@
+package com.neonxorg.neonx;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mWebView.loadUrl("https://yahoo.com");
+                    return true;
+                case R.id.navigation_dashboard:
+                    mWebView.loadUrl("https://qq.com");
+                    return true;
+                case R.id.navigation_notifications:
+                    mWebView.loadUrl("https://taobao.com");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mWebView = (WebView) findViewById(R.id.webkit);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webSettings.setDomStorageEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                mWebView.loadUrl("https://baidu.com");
+                return true;
+            }
+        });
+    }
+
+}
