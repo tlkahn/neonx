d600e2a17c004cb8dfd8469f9e5c8a7190bc4b20
toeinriver <toeinriver@gmail.com>
2019-08-02 15:43:17 +0800

    add data dir

---
+++ app/src/main/assets/www/precache-manifest.81af63d07b6dd6ae8e331187c522b020.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0418ec4afd8808b7e7a817faf75bd4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a58872d3e57eed61a1c3",
+    "url": "./static/css/2.b2cf1c3f.chunk.css"
+  },
+  {
+    "revision": "dd134dd18d2b3a9bfe4b",
+    "url": "./static/css/main.ce5735c2.chunk.css"
+  },
+  {
+    "revision": "a58872d3e57eed61a1c3",
+    "url": "./static/js/2.a509663d.chunk.js"
+  },
+  {
+    "revision": "dd134dd18d2b3a9bfe4b",
+    "url": "./static/js/main.4762cea0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "./static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "./static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "./static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "6814d0e8136d34e313623eb7129d538e",
+    "url": "./static/media/fa-brands-400.6814d0e8.woff2"
+  },
+  {
+    "revision": "83e6c29fb363b2f0ea6cc18fefff729c",
+    "url": "./static/media/fa-brands-400.83e6c29f.svg"
+  },
+  {
+    "revision": "da408238128b876cbda6424391f1566f",
+    "url": "./static/media/fa-brands-400.da408238.woff"
+  },
+  {
+    "revision": "e8019d507e8cb51d169ab4f94a0cda12",
+    "url": "./static/media/fa-brands-400.e8019d50.eot"
+  },
+  {
+    "revision": "fdf44bc43e8fa2358bbb7d9165d78455",
+    "url": "./static/media/fa-brands-400.fdf44bc4.ttf"
+  },
+  {
+    "revision": "8d220c793e2612bd131ed8522c54669f",
+    "url": "./static/media/fa-regular-400.8d220c79.ttf"
+  },
+  {
+    "revision": "8d9ab84bfe87a3f77112a6698cf639fb",
+    "url": "./static/media/fa-regular-400.8d9ab84b.woff2"
+  },
+  {
+    "revision": "ba2a91dc95e6cfdc4b2a186a7ba83e29",
+    "url": "./static/media/fa-regular-400.ba2a91dc.svg"
+  },
+  {
+    "revision": "dad90637f797356bbc70d2664832e0b6",
+    "url": "./static/media/fa-regular-400.dad90637.woff"
+  },
+  {
+    "revision": "e6c93cb47e716b579264a5fdfbf7e84d",
+    "url": "./static/media/fa-regular-400.e6c93cb4.eot"
+  },
+  {
+    "revision": "132e9759d93e4eefd7cdde0d7a322991",
+    "url": "./static/media/fa-solid-900.132e9759.ttf"
+  },
+  {
+    "revision": "2d0415fa29ea596b7a02c78eddeede20",
+    "url": "./static/media/fa-solid-900.2d0415fa.woff"
+  },
+  {
+    "revision": "b75b4bfe0d58faeced5006c785eaae23",
+    "url": "./static/media/fa-solid-900.b75b4bfe.woff2"
+  },
+  {
+    "revision": "de1d242d8acb26ec43c0d071fe78e72d",
+    "url": "./static/media/fa-solid-900.de1d242d.svg"
+  },
+  {
+    "revision": "ea363ed422723673917901680be9b37c",
+    "url": "./static/media/fa-solid-900.ea363ed4.eot"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "./static/media/ionicons.19e65b89.eot"
+  },
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "./static/media/ionicons.2c159d0d.woff"
+  },
+  {
+    "revision": "326445b510380aedd5b7c9711d8ca9d2",
+    "url": "./static/media/ionicons.326445b5.svg"
+  },
+  {
+    "revision": "379393493c3672e03017f8c4078afcd4",
+    "url": "./static/media/ionicons.37939349.ttf"
+  },
+  {
+    "revision": "3f2acd40149b3704f3a97364d0fed0d3",
+    "url": "./static/media/ionicons.3f2acd40.eot"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "./static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "aff28a207631f39ee0272d5cdde43ee7",
+    "url": "./static/media/ionicons.aff28a20.svg"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "./static/media/ionicons.dd4781d1.ttf"
+  },
+  {
+    "revision": "e74f7161730ced5656582f9faf231661",
+    "url": "./static/media/ionicons.e74f7161.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
--- app/src/main/java/com/neonxorg/neonx/MainActivity.java
+++ app/src/main/java/com/neonxorg/neonx/MainActivity.java
@@ -18,6 +18,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -99,15 +100,67 @@ public class MainActivity extends AppCompatActivity {
         startLocalServer(3000, "www", true, true );
     }
 
-  public void startLocalServer(int port, String root, Boolean localhost, Boolean keepAlive) {
+
+    public void startLocalServer(int port, String root, Boolean localhost, Boolean keepAlive) {
       try {
-          File www_root = new File(root);
-          server = new WebServer("localhost", port, www_root.getAbsoluteFile());
+          String[] filePathList =(getAssets().list("www"));
+          for (String s : filePathList) {
+              System.out.println(s);
+          }
+          MainActivity.copyFolderFromAssets(this.getApplicationContext(), "www", getApplicationInfo().dataDir + '/' + root);
+          File www_root = new File(getApplicationInfo().dataDir + '/' + root);
+          System.out.println(www_root.listFiles());
+          server = new WebServer("localhost", port, www_root.getCanonicalFile());
           server.start();
           printIp();
+
       } catch (IOException e) {
           e.printStackTrace();
       }
+  }
+
+  public static void copyFolderFromAssets(Context context, String rootDirFullPath, String targetDirFullPath) {
+        System.out.println("copyFolderFromAssets " + "rootDirFullPath-" + rootDirFullPath + " targetDirFullPath-" + targetDirFullPath);
+        File file = new File(targetDirFullPath);
+        if (!file.exists()) {
+          new File(targetDirFullPath).mkdirs();
+        }
+        try {
+            String[] listFiles = context.getAssets().list(rootDirFullPath);// 遍历该目录下的文件和文件夹
+            for (String string : listFiles) {// 看起子目录是文件还是文件夹，这里只好用.做区分了
+                if (isFileByName(string)) {// 文件
+                    copyFileFromAssets(context, rootDirFullPath + "/" + string, targetDirFullPath + "/" + string);
+                } else {// 文件夹
+                    String childRootDirFullPath = rootDirFullPath + "/" + string;
+                    String childTargetDirFullPath = targetDirFullPath + "/" + string;
+                    new File(childTargetDirFullPath).mkdirs();
+                    copyFolderFromAssets(context, childRootDirFullPath, childTargetDirFullPath);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+  public static void copyFileFromAssets(Context context, String assetsFilePath, String targetFileFullPath) {
+        InputStream assestsFileInputStream;
+        try {
+          assestsFileInputStream = context.getAssets().open(assetsFilePath);
+          FileOutputStream fOS = new FileOutputStream(new File(targetFileFullPath));
+          int length = -1;
+          byte[] buf = new byte[1024];
+          while ((length = assestsFileInputStream.read(buf)) != -1) {
+             fOS.write(buf, 0, length);
+          }
+          fOS.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+  private static boolean isFileByName(String str) {
+        return str.contains(".");
   }
 
   private void printIp() {
